4ecdd8af6363c0488515c8e364490a2c0511682e
rohan gunge <321rohan0011@dbit.in>
2023-10-07 16:21:17 +0530

    10-7-23

--- app.js
+++ app.js
@@ -11,3 +11,21 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
+
+function initMap() {
+  const mapOptions = {
+      center: { lat: YOUR_LATITUDE, lng: YOUR_LONGITUDE }, // Replace with your coordinates
+      zoom: 15 // Adjust the zoom level as needed
+  };
+  const map = new google.maps.Map(document.getElementById("map-container"), mapOptions);
+}
+
+// Load the Google Map when the page is loaded
+google.maps.event.addDomListener(window, "load", initMap);
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
